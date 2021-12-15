dd2e66615205b6b8cd9b37d1f99d3d438b752596
SergeiRest <59287617+SergeiRest@users.noreply.github.com>
2021-12-16 00:00:07 +0300

    Корректировка уничтожения

---
+++ Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+	[SerializeField] private GameObject _cubeContainer;
+	private PlayerTriggers _trigger;
+
+	private void Start()
+	{
+		_trigger = GetComponent<PlayerTriggers>();
+		_trigger.PlayerDeath += DeathAnimation;
+	}
+
+	private void DeathAnimation()
+	{
+		Instantiate(_cubeContainer, transform.position, Quaternion.identity);
+	}
+}
