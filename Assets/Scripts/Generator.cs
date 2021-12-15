e00a959c2b7a66776efbf5f597d4d4de7ddf2315
SergeiRest <59287617+SergeiRest@users.noreply.github.com>
2021-12-15 18:49:21 +0300

    Псевдо-генерация

---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+	[SerializeField] private GameObject _gamePlane;
+	[SerializeField] private GameObject _wall;
+	private int rand;
+	int xsc;
+	int zsc;
+    void Start()
+    {
+		xsc = -(int)(_gamePlane.transform.localScale.x - 1) / 2;
+		zsc = -(int)(_gamePlane.transform.localScale.z - 2) / 2;
+		for(int i = xsc; i < (_gamePlane.transform.localScale.x) / 2; i++)
+		{
+			for(int j = zsc; j < (_gamePlane.transform.localScale.z - 2) / 2; j++)
+			{
+				int rand = Random.Range(0, 2);
+				if(rand == 1)
+				{
+					var obj = Instantiate(_wall, new Vector3(i, 1, j), Quaternion.identity);
+				}
+				
+			}
+		}
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
